e7175d5bafc0ecc477e5bcf04ccc2341a464dab3
Pravin Devade <pravin.devade@asl.mum.sst>
2021-04-24 15:26:29 +0530

    Committing Project

---
+++ SampleProject/src/StringCheckData.java
@@ -0,0 +1,27 @@
+public class StringCheckData {
+	
+	public static void main(String[] args) {
+		
+		
+		String s1="Java";
+		String s2= new String("Java");
+		
+		//String s2= "Java";
+		
+		System.out.println("###Checking For euuality1111###");
+		
+		if(s1.equals(s2))
+			System.out.println("Equals Returned True");
+		else
+			System.out.println("Equals Returned False");
+		
+		
+		if(s1==s2)
+			System.out.println(" == Returned True");
+		else
+			System.out.println(" == Returned False");
+		
+		
+	}
+
+}
